dd43deee11109025c890c9f1f567db114d0f164f
Naoya Umeyama <n.ume0518@gmail.com>
2020-04-23 08:00:20 +0000

    PHP/Laravel 11の課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,23 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews1</title>
+        <title>MyProfile</title>
     </head>
     <body>
-        <h1>課題4のcreate</h1></h1>
+        <h1>プロフィールの作成</h1>
     </body>
 </html>
+
+@extends('layouts.profile')
+
+@section('title','プロフィール作成')
+
+@section('content')
+  <div class="container">
+      <div class="row">
+          <div class="col-md-8 mx-auto">
+              <h2>Myプロフィール</h2>
+          </div>
+      </div>
+  </div>
+@endsection
