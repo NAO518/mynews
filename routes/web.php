839d2c968b55c4dfde301da2f00f97ded6c35d99
Naoya Umeyama <n.ume0518@gmail.com>
2020-05-04 05:26:10 +0000

    PHP/Laravel 16の課題

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,13 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
     Route::get('news/create','Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth'); 
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); 
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
     
     Route::get('profile/create','Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\profileController@edit');
+    Route::get('profile/edit','Admin\ProfileController@edit');
     
     Route::post('profile/create','Admin\ProfileController@create');
     Route::post('profile/edit','Admin\ProfileController@update');
